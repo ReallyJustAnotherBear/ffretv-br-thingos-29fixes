526f16d88caf6696a2d325090e43330955deaacc
kelsieflynn <4595401+kelsieflynn@users.noreply.github.com>
2019-09-18 06:43:26 -0400

    sync with ffretv 20190918

--- package/ffretv/ffretv.mk
+++ package/ffretv/ffretv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Use a tag or a full commit ID
-FFRETV_VERSION = v29fixes20190917-5
+FFRETV_VERSION = v.29fixes20190917-6
 FFRETV_SOURCE = ffretv-$(FFRETV_VERSION).tar.gz
 FFRETV_SITE = $(call github,kelsieflynn,ffretv-br,$(FFRETV_VERSION))
 
